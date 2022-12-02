f3075c5f2e9da4699e634e70f00ad61092590655
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-12-02 17:39:08 +0000

    build only

--- src/classes/item/food/Ingredient.ts
+++
@@ -1,9 +0,0 @@
-import Food, { FoodOptions } from "./Food";
-
-export default class Ingredient extends Food {
-	constructor(options: FoodOptions) {
-		super(options);
-
-		this.type = "Ingredient";
-	}
-}
--- src/utils/Copy.ts
+++
@@ -1,5 +0,0 @@
-export default function Copy(value: any) {
-	if (value == undefined) return undefined;
-	if (value == null) return null;
-	return JSON.parse(JSON.stringify(value));
-}
